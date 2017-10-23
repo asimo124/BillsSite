7b4e08e4b27675e62c799100826820f11ea28a5b
asimo124 <Weebopze4!23>
2017-10-23 12:44:54 -0500

    changed to nice line chart with customizable hex color codes

--- sol/glucose/chart.php
+++ sol/glucose/chart.php
@@ -50,8 +50,8 @@ foreach ($resultset as $getItem) {
 
 
 
-$lastMonthName = date("M", strtotime($lastMonthDate));
-$curMonthName = date("M");
+$lastMonthName = date("F", strtotime($lastMonthDate));
+$curMonthName = date("F");
 ?>
 <!DOCTYPE html>
 <html>
@@ -63,8 +63,6 @@ $curMonthName = date("M");
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="/css/nav.css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/css/mdb.min.css" />
-    <link rel="stylesheet" href="/css/style_mdb.css" />
 </head>
 <body>
 <div class="container">
@@ -81,73 +79,137 @@ $curMonthName = date("M");
     <a href="index.php" >Back</a>
     <div style="clear: both; height: 7px;" ></div>
 
-
-    <canvas id="lineChart" colours="colours"></canvas>
-</div>
-
+    <canvas id="myChart" style="height: 370px; width: 100%;"></canvas>
+    <?php /*<div id="chartContainer" style="height: 370px; width: 100%;"></div>*/ ?>
 </div>
 </body>
 <script src="https://code.jquery.com/jquery.js"></script>
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.5/popper.min.js" ></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.4.1/js/mdb.min.js" ></script>
+<?php /*<script src="/js/jquery.canvasjs.min.js" ></script>*/ ?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.min.js" ></script>
 <script src="/js/nav.js" ></script>
 <script>
+    <?php/*
+    window.onload = function () {
 
-    $(document).ready(function() {
+//Better to construct options first and then pass it as a parameter
+        var options = {
+            responsive: true,
+            exportEnabled: true,
+            animationEnabled: true,
+            title: {
+                text: "<?php echo $curMonthName; ?> Glucose Levels"
+            },
+            data: [
+                {
+                    color: "orange",
+                    legendText: "Morning",
+                    showInLegend: true,
+                    type: "splineArea", //change it to line, area, bar, pie, etc
+                    dataPoints: [
+                        <?php foreach ($allResults as $date => $getItem) : ?>
+                            <?php if (isset($getItem['morning'])) { ?>
+                                { y: <?php echo $getItem['morning']['level']; ?>, label: "<?php echo date("jS", strtotime($getItem['morning']['date_taken'])); ?>"},
+                            <?php } else if (isset($getItem['evening'])) { ?>
+                                { y: null, label: "<?php echo date("jS", strtotime($getItem['evening']['date_taken'])); ?>"},
+                            <?php } ?>
+                        <?php endforeach; ?>
+                    ]
+                },
+                {
+                    color: "blue",
+                    legendText: "Evening",
+                    showInLegend: true,
+                    type: "splineArea", //change it to line, area, bar, pie, etc
+                    dataPoints: [
+                        <?php foreach ($allResults as $date => $getItem) : ?>
+                            <?php if (isset($getItem['evening'])) { ?>
+                                { y: <?php echo $getItem['evening']['level']; ?>, label: "<?php echo date("jS", strtotime($getItem['evening']['date_taken'])); ?>"},
+                            <?php } else if (isset($getItem['morning'])) { ?>
+                                { y: null, label: "<?php echo date("jS", strtotime($getItem['morning']['date_taken'])); ?>"},
+                            <?php } ?>
+                        <?php endforeach; ?>
+                    ]
+                }
+            ]
+        };
+        $("#chartContainer").CanvasJSChart(options);
 
-        var ctxL = document.getElementById("lineChart").getContext('2d');
-        var myLineChart = new Chart(ctxL, {
-            type: 'line',
-            data: {
-                labels: [
+    }*/ ?>
+
+    /*
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });*/
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [
+            <?php foreach ($allResults as $date => $getItem) : ?>
+                "<?php echo date("jS", strtotime($date)); ?>",
+            <?php endforeach; ?>,
+            ],
+            datasets: [{
+                label: 'Morning',
+                data: [
                     <?php foreach ($allResults as $date => $getItem) : ?>
-                        "<?php echo date("D, jS", strtotime($date)); ?>",
+                        <?php if (isset($getItem['morning'])) { ?>
+                            <?php echo $getItem['morning']['level']; ?>,
+                        <?php } else if (isset($getItem['evening'])) { ?>
+                            null,
+                        <?php } ?>
                     <?php endforeach; ?>
                 ],
-                datasets: [
-                    {
-                        label: "<?php echo $curMonthName; ?> Levels in Morning",
-                        fillColor: "rgba(252,196,128,0.2)",
-                        strokeColor: "rgba(237,139,16,1)",
-                        pointColor: "rgba(242,110,110,1)",
-                        pointStrokeColor: "#ed8b10",
-                        pointHighlightFill: "#ed8b10",
-                        pointHighlightStroke: "rgba(237,139,16,1)",
-                        data: [
-                            <?php foreach ($allResults as $date => $getItem) : ?>
-                                <?php if (isset($getItem['morning'])) : ?>
-                                    <?php echo $getItem['morning']['level']; ?>,
-                                <?php /*elseif (isset($getItem['evening'])) : ?>
-                                    <?php echo $getItem['evening']['level'];*/ ?>
-                                <?php endif; ?>
-                            <?php endforeach; ?>
-                        ]
-                    },
-                    {
-                        label: "<?php echo $curMonthName; ?> Levels in Evening",
-                        fillColor: "rgba(252,196,128,0.2)",
-                        strokeColor: "rgba(237,139,16,1)",
-                        pointColor: "rgba(242,110,110,1)",
-                        pointStrokeColor: "#ed8b10",
-                        pointHighlightFill: "#ed8b10",
-                        pointHighlightStroke: "rgba(237,139,16,1)",
-                        data: [
-                            <?php foreach ($allResults as $date => $getItem) : ?>
-                                <?php if (isset($getItem['evening'])) : ?>
-                                    <?php echo $getItem['evening']['level']; ?>,
-                                <?php /*elseif (isset($getItem['morning'])) : ?>
-                                    <?php echo $getItem['morning']['level'];*/ ?>
-                                <?php endif; ?>
-                            <?php endforeach; ?>
-                        ]
-                    }
-                ]
-            },
-            options: {
-                responsive: true
-            }
-        });
-
-    })
+                backgroundColor: "rgba(237,139,16,0.4)"
+            }, {
+                label: 'Evening',
+                data: [
+                    <?php foreach ($allResults as $date => $getItem) : ?>
+                        <?php if (isset($getItem['evening'])) { ?>
+                            <?php echo $getItem['evening']['level']; ?>,
+                        <?php } else if (isset($getItem['morning'])) { ?>
+                            null,
+                        <?php } ?>
+                    <?php endforeach; ?>
+                ],
+                backgroundColor: "rgba(68,138,252,0.4)"
+            }]
+        }
+    });
 </script>
