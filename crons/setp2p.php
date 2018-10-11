78af4be5b36697d747e45da8dc00fec664f5cc7c
Alex Hawley <asimo124@yahoo.com>
2018-10-10 20:39:48 -0500

    Changed p2p to 9 months (between 6 and 8 months)

--- crons/setp2p.php
+++ crons/setp2p.php
@@ -2,12 +2,12 @@
 ini_set("display_errors", "1");
 require_once("../inc/includes.php");
 
-$set_opt = isset($_REQUEST['set_opt']) ? true : false;
+$set9 = isset($_REQUEST['set9']) ? true : false;
 $set_pes = isset($_REQUEST['set_pes']) ? true : false;
 
-if ($set_opt || $set_pes) {
+if ($set9) {
 
-    $date3More = date("Y-m-d 19:00:00", strtotime("+3 months"));
+    $date3More = date("Y-m-d 19:00:00", strtotime("+8 months"));
 
     $datesArr = array();
     $date3More = date("Y-m-d H:i:s", strtotime($date3More . " -1 days"));
@@ -27,119 +27,13 @@ if ($set_opt || $set_pes) {
             }
             $datesArr[$weekOfYear][] = $nextDate;
 
-            if (count($datesArr) >= 24 && $dayOfWeek == 3) {
+            if (count($datesArr) >= 8 && $dayOfWeek == 3) {
                 break;
             }
         }
     }
 
-    $rand_num = rand(0, 100);
-    if ($set_opt) {  // later
-
-        $week = 1;
-        if ($rand_num >= 100) {
-            $week = 0;
-        } else if ($rand_num >= 94) {
-            $week = 1;
-        } else if ($rand_num >= 88) {
-            $week = 2;
-        } else if ($rand_num >= 83) {
-            $week = 3;
-        } else if ($rand_num >= 78) {
-            $week = 4;
-        } else if ($rand_num >= 73) {
-            $week = 5;
-        } else if ($rand_num >= 68) {
-            $week = 6;
-        } else if ($rand_num >= 63) {
-            $week = 7;
-        } else if ($rand_num >= 58) {
-            $week = 8;
-        } else if ($rand_num >= 54) {
-            $week = 9;
-        } else if ($rand_num >= 50) {
-            $week = 10;
-        } else if ($rand_num >= 46) {
-            $week = 11;
-        } else if ($rand_num >= 42) {
-            $week = 12;
-        } else if ($rand_num >= 38) {
-            $week = 13;
-        } else if ($rand_num >= 34) {
-            $week = 14;
-        } else if ($rand_num >= 30) {
-            $week = 15;
-        } else if ($rand_num >= 26) {
-            $week = 16;
-        } else if ($rand_num >= 22) {
-            $week = 17;
-        } else if ($rand_num >= 18) {
-            $week = 18;
-        } else if ($rand_num >= 14) {
-            $week = 19;
-        } else if ($rand_num >= 11) {
-            $week = 20;
-        } else if ($rand_num >= 7) {
-            $week = 21;
-        } else if ($rand_num >= 3) {
-            $week = 22;
-        } else {                     // 2
-            $week = 23;
-        }
-
-    } else {  // set later
-
-        $week = 23;
-        if ($rand_num >= 100) {
-            $week = 23;
-        } else if ($rand_num >= 94) {
-            $week = 22;
-        } else if ($rand_num >= 88) {
-            $week = 21;
-        } else if ($rand_num >= 83) {
-            $week = 20;
-        } else if ($rand_num >= 78) {
-            $week = 19;
-        } else if ($rand_num >= 73) {
-            $week = 18;
-        } else if ($rand_num >= 68) {
-            $week = 17;
-        } else if ($rand_num >= 63) {
-            $week = 16;
-        } else if ($rand_num >= 58) {
-            $week = 15;
-        } else if ($rand_num >= 54) {
-            $week = 14;
-        } else if ($rand_num >= 50) {
-            $week = 13;
-        } else if ($rand_num >= 46) {
-            $week = 12;
-        } else if ($rand_num >= 42) {
-            $week = 11;
-        } else if ($rand_num >= 38) {
-            $week = 10;
-        } else if ($rand_num >= 34) {
-            $week = 9;
-        } else if ($rand_num >= 30) {
-            $week = 8;
-        } else if ($rand_num >= 26) {
-            $week = 7;
-        } else if ($rand_num >= 22) {
-            $week = 6;
-        } else if ($rand_num >= 18) {
-            $week = 5;
-        } else if ($rand_num >= 14) {
-            $week = 4;
-        } else if ($rand_num >= 11) {
-            $week = 3;
-        } else if ($rand_num >= 7) {
-            $week = 2;
-        } else if ($rand_num >= 3) {
-            $week = 1;
-        } else {
-            $week = 0;
-        }
-    }
+    $week = rand(0, 7);
 
     $day = rand(0, 2);
     $nextDate = $datesArr[$months[$week]][$day];
@@ -168,12 +62,12 @@ if ($set_opt || $set_pes) {
 <body>
     <div style="margin: 45px auto; width: 1152px;" >
         <form action="setp2p.php" method="post">
-            <button type="submit" name="set_opt" id="set_opt" value="" >Set Optimistic</button>
+            <button type="submit" name="set9" id="set9" value="" >Set 9 Months</button>
         </form>
         <div style="clear: both; height: 12px;" ></div>
-        <form action="setp2p.php" method="post">
-            <button type="submit" name="set_pes" id="set_pes" value="" >Set Pessimistic</button>
-        </form>
+<!--        <form action="setp2p.php" method="post">-->
+<!--            <button type="submit" name="set_pes" id="set_pes" value="" >Set Pessimistic</button>-->
+<!--        </form>-->
     </div>
 </body>
 </html>
