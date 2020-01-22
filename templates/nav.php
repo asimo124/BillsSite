3832271af0369966c5fe5a4b52406176c1bd732a
alex hawley <alex@engage2learn.org>
2020-01-22 08:19:56 -0600

    - Fixed links since we are now using Angular on the home page

--- templates/nav.php
+++ templates/nav.php
@@ -23,13 +23,13 @@
 </nav>-->
 
 <div class="topnav" id="myTopnav">
-    <a href="/bills">Bills</a>
-    <a href="/expenses">Add Expense</a>
-    <a href="/bills/admin">Bills Admin</a>
+    <a href="/bills/index.php">Bills</a>
+    <a href="/expenses/index.php">Add Expense</a>
+    <a href="/bills/admin/index.php">Bills Admin</a>
     <a href="/charges/stacked_chart.php">Charges Chart</a>
     <a href="/charges/upload.php">Upload Charges</a>
     <a href="/charges/categorize.php">Categorize Charges</a>
-    <a href="/charges/cats">Charge Categories</a>
+    <a href="/charges/cats/index.php">Charge Categories</a>
     <a href="/charges/manage_desc.php">Manage Desc</a>
     <a href="/logout.php">Logout</a>
     <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
