360e1d6082f0370282e6f908682968c35cf052a3
Alex Hawley <asimo124@yahoo.com>
2025-10-02 18:23:45 -0500

    - Documented more on API design. Updated UI to match what it needs to do

--- templates/nav.php
+++ templates/nav.php
@@ -27,8 +27,9 @@
     <a href="/expenses/index.php">Add Expense</a>
     <a href="/bills/admin/index.php">Bills Admin</a>
     <a href="/bills/admin/upcoming_expenses.php">Upcoming Expenses</a>
-    <a href="/bills/admin/budget_progress.php">Budget Progress</a>
+    <a href="/bills/admin/budget_progress2.php">Budget Progress</a>
     <a href="/bills/admin/budget_track.php">Track Progress</a>
+    <a href="/bills/admin/income_purchases.php">Income Purchases</a>
     <a href="/bills/admin/queue_date_job.php">Queue Date Job</a>
     <a href="/bills/admin/pending_transactions.php">Pending Transactions</a>
     <a href="/settings/index.php">Settings</a>
