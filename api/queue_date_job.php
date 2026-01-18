5f2a84f78cd1b8c71b85affd6a40ccca4e623e60
Alex Hawley <asimo124@yahoo.com>
2026-01-18 17:50:48 -0600

    - Fixed issue where nightly cron wasn't doing what we needed any more

--- api/queue_date_job.php
+++ api/queue_date_job.php
@@ -1,10 +1,15 @@
 <?php
 $changeTestMode            = isset($_REQUEST['test_mode']) ? intval($_REQUEST['test_mode']) : 0;
-include "../inc/Bills.php";
-include "../inc/includes.php";
+
+$base_path = dirname(__FILE__) . "/../";
+session_start();
+
+include $base_path . "inc/Bills.php";
+include $base_path . "inc/includes.php";
 ini_set("display_errors", 1);
 
-if (!isset($_SESSION['user'])) {
+
+if (php_sapi_name() !== 'cli' && !isset($_SESSION['user'])) {
     echo "Not logged in\n";
     exit;
 }
